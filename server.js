d476d9fa9b248eea75a0f1b42fd339f7cfadb2f7
saharsh <puramsaharsh@gmail.com>
2023-09-29 00:35:21 -0500

    cors fixed

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const products = require('./db/products')
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.get('/', (req, res) => {
